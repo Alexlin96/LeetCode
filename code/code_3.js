cbb38e4fbe3a96f55c6c11e768bb10cc73411bb0
linzhiqiang <linzhiqiang@goatgames.com>
2021-04-08 16:20:42 +0800

    xxx

--- code/code_3.js
+++ code/code_3.js
@@ -27,3 +27,22 @@ var findRestaurant = function (list1, list2) {
   }
   return num === -1 ? "" : res;
 };
+
+/*
+ * @title: 存在重复元素 II (219)
+ * @description: 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k
+ * @keyword: 数组元素对比
+ * @date: 2021-03-22
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  const arr = []
+  for (let i = 0; i < nums.length; i++) {
+    if (arr.includes(nums[i])) { // 包含相同 返回true
+      return true
+    } else {
+      arr.push(nums[i])
+    }
+    if (arr.length > k) arr.shift() // 长度大于k 删除第一个元素
+  }
+  return false
+};
