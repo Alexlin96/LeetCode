57024e25125a8680802edf8c9de4f215b2042442
linzhiqiang <linzhiqiang@goatgames.com>
2021-04-14 18:33:52 +0800

    xxx

--- code/code_3.js
+++ code/code_3.js
@@ -46,3 +46,86 @@ var containsNearbyDuplicate = function(nums, k) {
   }
   return false
 };
+
+/*
+ * @title: 字母异位词分组 (49)
+ * @description: 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串
+ * @keyword: hash 字符串排序
+ * @date: 2021-03-23
+ */
+var groupAnagrams = function(strs) {
+  let hash = {}
+  for (let i = 0; i < strs.length; i++) {
+    let newItem = Array.from(strs[i])
+    newItem.sort() // 排序 按元素Unicode位点排序，即Unicode编码升序排列
+    newItem = newItem.join('')
+    if (!hash[newItem]) {
+      hash[newItem] = [strs[i]]
+    } else {
+      hash[newItem].push(strs[i])
+    }
+  }
+  return Object.values(hash)
+};
+
+/*
+ * @title: 宝石与石头 (771)
+ * @description:  给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石
+ * @keyword: hash
+ * @date: 2021-03-24
+ * @param {*} jewels 宝石的类型
+ * @param {*} stones 石头的类型
+ */
+var numJewelsInStones = function(jewels, stones) {
+  let stonesArr = Array.from(stones)
+  let num = 0
+  for(const i of stonesArr) {
+    jewels.includes(i) && (num += 1)
+  }
+  return num
+};
+
+/*
+ * @title: 四数相加 II (454)
+ * @description: 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+ * @keyword: hash 取反对比
+ * @date: 2021-03-25
+ */
+var fourSumCount = function(nums1, nums2, nums3, nums4) {
+  let hash_1 = {}
+  let num = 0
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (!hash_1[nums1[i]+nums2[j]]) {
+        hash_1[nums1[i]+nums2[j]] = 1
+      } else {
+        hash_1[nums1[i]+nums2[j]] += 1
+      }
+    }
+  }
+  for (let k = 0; k < nums3.length; k++) {
+    for (let h = 0; h < nums4.length; h++) {
+      const diffKey = String(-(nums3[k] + nums4[h])) 
+      hash_1.hasOwnProperty(diffKey) && (num+=hash_1[diffKey]) // 判断是否等于相反数
+    }
+  }
+  return num
+};
+
+/*
+ * @title: 前 K 个高频元素 (347)
+ * @description: 给定一个非空的整数数组，返回其中出现频率前 k 高的元素
+ * @keyword: hash 排序
+ * @date: 2021-03-26
+ */
+var topKFrequent = function(nums, k) {
+  let hash = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1
+    } else {
+      hash[nums[i]] += 1
+    }
+  }
+  return Object.entries(hash).sort((a, b) => b[1] - a[1]).slice(0, k).map(a => a[0]);
+};
